eeffca37db81f25163429a636bd53fc884952e99
Lunamorgendorffer <nabdou68@gmail.com>
2023-03-06 11:10:57 +0100

    Ajout fonction askNumberBoxes

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
+//fonction pour demander le nombre de boîtes de départ au joueur
+function askNumberBoxes(){
+    //On demande à l'utilisateur de saisir le nombre de boîtes qu'il souhaite utiliser pour jouer
+    let numberBoxes = prompt ("Combien de boites voules vous utiliser pour jouer ?")
+    while (!numberBoxes || isNaN(numberBoxes)){
+        //Si l'entrée n'est pas valide, la fonction redemande à l'utilisateur de saisir un nombre valide.
+        numberBoxes = prompt("Veuillez entrer un nombre valide de boites :")
+    }
+    return parseInt(numberBoxes)
+}
+
 
 function shuffleChildren(parent){
     let children = parent.children
@@ -24,13 +35,17 @@ function showReaction(type,clickedBox){
     }
 }
 
+
 const box = document.createElement("div");
 box.classList.add("box");
 
 const board = document.querySelector("#board");
 let nb =1
 
-for(let i= 1; i <= 10; i++){
+let numberBoxes = askNumberBoxes();
+
+//On demande au joueur de spécifier le nombre de boîtes de départ qu'il souhaite,
+for(let i= 1; i <= numberBoxes; i++){
     const newbox = box.cloneNode()
     newbox.innerText = i 
     board.appendChild(newbox)
